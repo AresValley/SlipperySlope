ae5561a51ab9a104ebca5fcc1f426e0b086a5885
Marco <marcodallatiezza@gmail.com>
2019-08-29 17:31:08 +0200

    Basolo slippage delta, code refactoring

--- SlipperySlope.py
+++ SlipperySlope.py
@@ -1,8 +1,8 @@
-"""LISP Calculator"""
+"""Slippage Calculator"""
 import sys
 import numpy as np
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 
 
 class Molecule:
@@ -33,26 +33,26 @@ def dist(atom_a, atom_b):
 
 def ang(atom_a, atom_b, atom_c):
     """Calulate the angle given three atoms"""
-    ba = atom_a - atom_b
-    bc = atom_c - atom_b
+    b_a = atom_a - atom_b
+    b_c = atom_c - atom_b
     rad_angle = np.arccos(
-        np.dot(ba, bc) / (dist(atom_a, atom_b) * dist(atom_b, atom_c))
+        np.dot(b_a, b_c) / (dist(atom_a, atom_b) * dist(atom_b, atom_c))
     )
     return rad_angle
 
 
-def lisp(ring_coor, M_coor):
+def lisp(ring_coor, m_coor):
     """Calculate the Label Independednt Slippage Parameter"""
     ring_ctd = np.mean(ring_coor, axis=0)
-    d_ring_ctd_M = dist(ring_ctd, M_coor)
+    d_ring_ctd_m = dist(ring_ctd, m_coor)
     d_list_norm = []
     for idx, val in enumerate(ring_coor):
         if idx != len(ring_coor)-1:
             d_ring_pair_mid = np.mean(
                 [ring_coor[idx], ring_coor[idx+1]], axis=0)
             print("<{}-{}>...\t\t{:.6f}  {:.6f}  {:.6f}".format(
-                ring_idx[idx] + 1,
-                ring_idx[idx+1] + 1,
+                RING_IDX[idx] + 1,
+                RING_IDX[idx+1] + 1,
                 d_ring_pair_mid[0],
                 d_ring_pair_mid[1],
                 d_ring_pair_mid[2]
@@ -60,17 +60,17 @@ def lisp(ring_coor, M_coor):
         else:
             d_ring_pair_mid = np.mean([ring_coor[idx], ring_coor[0]], axis=0)
             print("<{}-{}>...\t\t{:.6f}  {:.6f}  {:.6f}".format(
-                ring_idx[idx] + 1,
-                ring_idx[0] + 1,
+                RING_IDX[idx] + 1,
+                RING_IDX[0] + 1,
                 d_ring_pair_mid[0],
                 d_ring_pair_mid[1],
                 d_ring_pair_mid[2]
                 ))
 
-        theta = ang(d_ring_pair_mid, ring_ctd, M_coor)
-        d_list_norm.append(abs(d_ring_ctd_M*np.sin(theta-np.pi/2)))
+        theta = ang(d_ring_pair_mid, ring_ctd, m_coor)
+        d_list_norm.append(abs(d_ring_ctd_m*np.sin(theta-np.pi/2)))
 
-    return np.mean(d_list_norm), ring_ctd, d_ring_ctd_M
+    return np.mean(d_list_norm), ring_ctd, d_ring_ctd_m
 
 print("""
     ================================
@@ -83,8 +83,8 @@ print("Load molecule...\tDONE!\n")
 
 print("--> INPUT\n")
 try:
-    ring_idx = [int(i)-1 for i in input("Index for the ring...\t").split()]
-    M_idx = int(input("Index for the M atom...\t"))-1
+    RING_IDX = [int(i)-1 for i in input("Index for the ring...\t").split()]
+    M_IDX = int(input("Index for the M atom...\t"))-1
 except ValueError as val_err:
     print("The input index is not valid: ", val_err)
     quit()
@@ -93,30 +93,30 @@ print("\n--> SANITY CHECK\n")
 
 try:
     print("Ring definition...\t\t", end="")
-    if len(ring_idx) >= 3:
+    if len(RING_IDX) >= 3:
         print("OK!")
     else:
         print("ERROR!")
         raise ValueError()
 
-    d_list = []
-    for idx, val in enumerate(ring_idx):
-        if idx != len(ring_idx)-1:
-            d_list.append(
-                dist(MOL.xyz[val], MOL.xyz[ring_idx[idx+1]])
+    D_LIST = []
+    for idx, val in enumerate(RING_IDX):
+        if idx != len(RING_IDX)-1:
+            D_LIST.append(
+                dist(MOL.xyz[val], MOL.xyz[RING_IDX[idx+1]])
             )
         else:
-            d_list.append(
-                dist(MOL.xyz[val], MOL.xyz[ring_idx[0]])
+            D_LIST.append(
+                dist(MOL.xyz[val], MOL.xyz[RING_IDX[0]])
             )
-    d_list_var = np.var(d_list)
-    d_list_std = np.std(d_list)
-    print("Variance...\t\t{:.3f}".format(d_list_var))
+    D_LIST_VAR = np.var(D_LIST)
+    D_LIST_STD = np.std(D_LIST)
+    print("Variance...\t\t{:.3f}".format(D_LIST_VAR))
     print("Std. deviation...\t", end="")
-    if d_list_std < 0.1:
-        print("{:.3f}\tOK!".format(d_list_std))
+    if D_LIST_STD < 0.1:
+        print("{:.3f}\tOK!".format(D_LIST_STD))
     else:
-        print("{:.3f}\tTOO HIGH!".format(d_list_std))
+        print("{:.3f}\tTOO HIGH!".format(D_LIST_STD))
         raise ValueError()
 
 except ValueError:
@@ -129,12 +129,64 @@ except ValueError:
     quit()
 
 print("\n--> LISP CALCULATION\n")
-ring_coor = [MOL.xyz[i] for i in ring_idx]
-LISP, ring_ctd, d_ring_ctd_M = lisp(ring_coor, MOL.xyz[M_idx])
+RING_COOR = [MOL.xyz[i] for i in RING_IDX]
+LISP, RING_CTD, D_RING_CTD_M = lisp(RING_COOR, MOL.xyz[M_IDX])
 
-print("\nRing centroid...\t{:.6f}  {:.6f}  {:.6f}".format(ring_ctd[0], ring_ctd[1], ring_ctd[2]))
-print("Dist. centroid-M...\t{:.3f}".format(d_ring_ctd_M))
+print("\nRing centroid...\t{:.6f}  {:.6f}  {:.6f}".format(RING_CTD[0], RING_CTD[1], RING_CTD[2]))
+print("Dist. centroid-M...\t{:.3f}".format(D_RING_CTD_M))
 print("LISP...\t\t\t{:.3f}".format(LISP))
+
+print("\n--> BASOLO CALCULATION\n")
+D_LIST_M_RING = [dist(MOL.xyz[M_IDX], i) for i in RING_COOR]
+
+L_SORT = sorted((e, i) for i, e in enumerate(D_LIST_M_RING))
+LONG_1 = L_SORT[-1]
+LONG_2 = L_SORT[-2]
+
+print("Longer C-M vicinity check...\t", end="")
+
+if LONG_2[1] == LONG_1[1] + 1:
+    if LONG_1[1] - 1 == -1:
+        ADJ_1_IDX = len(L_SORT) - 1
+    else:
+        ADJ_1_IDX = LONG_1[1] - 1
+
+    if LONG_2[1] + 1 == len(L_SORT):
+        ADJ_2_IDX = 0
+    else:
+        ADJ_2_IDX = LONG_2[1] + 1
+    print("OK!")
+elif LONG_2[1] == LONG_1[1] - 1:
+    if LONG_2[1] - 1 == -1:
+        ADJ_2_IDX = len(L_SORT) - 1
+    else:
+        ADJ_2_IDX = LONG_2[1] - 1
+
+    if LONG_1[1] + 1 == len(L_SORT):
+        ADJ_1_IDX = 0
+    else:
+        ADJ_1_IDX = LONG_1[1] + 1
+    print("OK!")
+elif LONG_1[1] + 1 == len(L_SORT) and LONG_2[1] == 0:
+    ADJ_1_IDX = LONG_1[1] - 1
+    ADJ_2_IDX = 1
+    print("OK!")
+elif LONG_2[1] + 1 == len(L_SORT) and LONG_1[1] == 0:
+    ADJ_1_IDX = LONG_1[1] + 1
+    ADJ_2_IDX = LONG_2[1] - 1
+    print("OK!")
+else:
+    print("FAILED!\nThe two longer C-M carbon atoms are not consecutive.\n Basolo Delta cannot be calcualted.")
+    BASOLO_DELTA = "N/D"
+
+BASOLO_DELTA = ((LONG_1[0]+LONG_2[0])-(D_LIST_M_RING[ADJ_1_IDX]+D_LIST_M_RING[ADJ_2_IDX]))/2
+
+if BASOLO_DELTA == "N/D":
+    print("Basolo Delta...\t\t\t{}".format(BASOLO_DELTA))
+else:
+    print("Long Bond:\t\t\tC{}-M, C{}-M".format(RING_IDX[LONG_1[1]]+1, RING_IDX[LONG_2[1]]+1))
+    print("Neighboring Bond:\t\tC{}-M, C{}-M".format(RING_IDX[ADJ_1_IDX]+1, RING_IDX[ADJ_2_IDX]+1))
+    print("Basolo Delta...\t\t\t{:.3f}".format(BASOLO_DELTA))
 
 print("""
     ================================
